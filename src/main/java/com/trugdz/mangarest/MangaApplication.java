942fe9473c077c1f25b489eae74230fb123823f8
Felipe Melo <felipe.leandro2@gmail.com>
2023-02-13 02:20:26 -0300

    create projetc

---
+++ src/main/java/com/trugdz/mangarest/MangaApplication.java
@@ -0,0 +1,13 @@
+package com.trugdz.mangarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaApplication.class, args);
+	}
+
+}
